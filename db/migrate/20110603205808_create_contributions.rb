d7f1ddccf9a2dad427bc0d7cf5efd41e74882a6e
C R H <cinjon@alum.mit.edu>
2011-07-12 01:12:52 -0400

    contributions with works

--- db/migrate/20110603205808_create_contributions.rb
+++ db/migrate/20110603205808_create_contributions.rb
@@ -3,11 +3,13 @@ class CreateContributions < ActiveRecord::Migration
     create_table :contributions do |t|
       t.integer :contributed_id
       t.integer :contributor_id
+      t.integer :work_id
 
       t.timestamps
     end
     add_index :contributions, :contributed_id
     add_index :contributions, :contributor_id
+    add_index :contributions, :work_id
   end
 
   def self.down
