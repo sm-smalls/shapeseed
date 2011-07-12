d249b82c1fbd5272526b0f19301a3c353329cbdf
C R H <cinjon@alum.mit.edu>
2011-07-12 02:14:05 -0400

    bunch with layout and such. chug

--- db/schema.rb
+++ db/schema.rb
@@ -13,29 +13,31 @@
 ActiveRecord::Schema.define(:version => 20110710171620) do
 
   create_table "categories", :force => true do |t|
-    t.string    "name"
-    t.integer   "person_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.integer  "person_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "categories", ["person_id"], :name => "index_categories_on_person_id"
 
   create_table "contributions", :force => true do |t|
-    t.integer   "contributed_id"
-    t.integer   "contributor_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "contributed_id"
+    t.integer  "contributor_id"
+    t.integer  "work_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "contributions", ["contributed_id"], :name => "index_contributions_on_contributed_id"
   add_index "contributions", ["contributor_id"], :name => "index_contributions_on_contributor_id"
+  add_index "contributions", ["work_id"], :name => "index_contributions_on_work_id"
 
   create_table "mentorships", :force => true do |t|
-    t.integer   "mentor_id"
-    t.integer   "mentee_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "mentor_id"
+    t.integer  "mentee_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "mentorships", ["mentee_id"], :name => "index_mentorships_on_mentee_id"
@@ -43,20 +45,20 @@ ActiveRecord::Schema.define(:version => 20110710171620) do
   add_index "mentorships", ["mentor_id"], :name => "index_mentorships_on_mentor_id"
 
   create_table "people", :force => true do |t|
-    t.string    "first"
-    t.string    "last"
-    t.text      "title"
-    t.text      "description"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.string    "name"
+    t.string   "first"
+    t.string   "last"
+    t.text     "title",       :limit => 255
+    t.text     "description", :limit => 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "relationships", :force => true do |t|
-    t.integer   "follower_id"
-    t.integer   "followed_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
@@ -64,41 +66,41 @@ ActiveRecord::Schema.define(:version => 20110710171620) do
   add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
 
   create_table "searches", :force => true do |t|
-    t.string    "query"
-    t.integer   "min_age"
-    t.integer   "max_age"
-    t.string    "work_results"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "query"
+    t.integer  "min_age"
+    t.integer  "max_age"
+    t.string   "work_results"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "tags", :force => true do |t|
-    t.string    "name"
-    t.integer   "work_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.integer  "work_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "tags", ["work_id"], :name => "index_tags_on_work_id"
 
   create_table "users", :force => true do |t|
-    t.string    "first"
-    t.string    "last"
-    t.string    "email",                                 :null => false
-    t.string    "crypted_password",                      :null => false
-    t.string    "password_salt",                         :null => false
-    t.string    "persistence_token",                     :null => false
-    t.string    "perishable_token",                      :null => false
-    t.integer   "login_count",        :default => 0,     :null => false
-    t.integer   "failed_login_count", :default => 0,     :null => false
-    t.timestamp "last_request_at"
-    t.timestamp "current_login_at"
-    t.timestamp "last_login_at"
-    t.string    "current_login_ip"
-    t.string    "last_login_ip"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.boolean   "admin",              :default => false
+    t.string   "first"
+    t.string   "last"
+    t.string   "email",                                 :null => false
+    t.string   "crypted_password",                      :null => false
+    t.string   "password_salt",                         :null => false
+    t.string   "persistence_token",                     :null => false
+    t.string   "perishable_token",                      :null => false
+    t.integer  "login_count",        :default => 0,     :null => false
+    t.integer  "failed_login_count", :default => 0,     :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
@@ -106,13 +108,13 @@ ActiveRecord::Schema.define(:version => 20110710171620) do
   add_index "users", ["persistence_token"], :name => "index_users_on_persistence_token", :unique => true
 
   create_table "works", :force => true do |t|
-    t.text      "content"
-    t.integer   "person_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "age"
-    t.string    "name"
-    t.text      "source"
+    t.text     "content"
+    t.integer  "person_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "age"
+    t.string   "name"
+    t.text     "source"
   end
 
   add_index "works", ["person_id"], :name => "index_works_on_person_id"
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,120 +1,17 @@
-require 'faker'
 
 namespace :db do
-  desc "Fill database with sample data"
+  desc "Put in Admin"
   task :populate => :environment do
     Rake::Task['db:reset'].invoke
-    make_admin
     make_users
-    make_people
-    make_works
-    make_relationships
-    #make_contributions
-    make_mentorships
-    make_tags
-    make_categories
   end
-end
-
-def make_admin
-  admin = User.create!(:first => "Cinjon",
-  					   :last  => "Resnick",
-  			 		   :email => "cinjon@resnick.com",
-			 		   :password => "P4ssw0rd",
-			 		   :password_confirmation => "P4ssw0rd")
-  admin.toggle!(:admin) #makes admin into an ... admin
 end
 
 def make_users
-  100.times do |n|
-    first_name  = Faker::Name.first_name
-    last_name   = Faker::Name.last_name
-    email = "user-#{n+1}@example.org"
-    password  = "P4ssw0rd"
-    user = User.create!(:first => first_name,
-    			 		:last => last_name,
-			     		:email => email,
-			     		:password => password,
-			     		:password_confirmation => password)
-  end
+  admin = User.create!(:first => "Cinjon",
+  					   :last  => "Resnick",
+  			 		   :email => "cinjon.resnick@gmail.com",
+			 		   :password => "966S41nt",
+			 		   :password_confirmation => "966S41nt")
+  admin.toggle!(:admin) #makes admin into an ... admin
 end
-
-def make_people
-  users = User.all
-  15.times do |n|
-    first 		= Faker::Name.first_name
-    last  		= Faker::Name.last_name
-    description = Faker::Lorem.sentence(2)
-    title 		= Faker::Lorem.sentence(2)
-    user 		= users[rand(users.length)]
-    person 		= Person.create!(:first => first,
-    			   				 :last  => last,
-    			   				 :description => description,
-    			  				 :title => title)
-    user.contribute!(person)
-  end    
-end
-
-def make_works
-  Person.all(:limit => 15).each do |person|
-    7.times do |a|
-      maxAge = 45
-      age = 5 + rand(maxAge - 5)
-      person.works.create!(:content => Faker::Lorem.sentence(3), 
-        				   :age => age,
-        				   :name => Faker::Lorem.sentence(1))
-    end
-  end
-end
-
-def make_tags
-  Person.all(:limit => 15).each do |person|
-    person.works.each do |work|
-      (rand (3)).times do |a|
-        work.tags.create!(:name => "#{work.id}tag")
-      end
-    end
-  end
-end
-
-def make_categories
-  Person.all(:limit => 15).each do |person|
-    (rand (3)).times do |a|
-      person.categories.create!(:name => "#{person.id}category")
-    end
-  end
-end
-
-def make_relationships 
-  users = User.all
-  user = users.first
-  following = users[1..30]
-  followers = users[3..20]
-  following.each { |followed| user.follow!(followed) }
-  followers.each { |follower| follower.follow!(user) }
-end
-
-def make_contributions
-  users = User.all
-  Person.all.each do |mentor|
-    (rand(7)).times do |a|
-      index = rand(users.length)
-      users[index].contribute!(mentor)
-    end
-  end
-end
-
-def make_mentorships
-  users = User.all
-  Person.all(:limit => 25).each do |person|
-    (rand(3) + 1).times do |a|
-      user = users[rand(users.length)]
-      if !user.mentoree?(person)
-        user.mentee!(person)
-      end
-    end
-  end
-end
-      
-
-    
