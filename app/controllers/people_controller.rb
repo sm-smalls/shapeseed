c9d9bb1fdbfb4f9d03eb870d3b5230cdda0c44a6
sm-access <cinjon@alum.mit.edu>
2011-07-18 09:11:40 -0400

    from bus ride

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,28 +6,28 @@ class PeopleController < ApplicationController
     @timeline_works_js = @person.works.reject{|work| work.blank?}
     @user = current_user
     @search = Search.new
-    #@works = @person.works.paginate(:page => params[:page])
     @signed_bool = true unless !@user
     @timeline_tags_js = @person.works.collect{|work| work.tags.map(&:name).join(" ")}
     @tl_length = @timeline_works_js.length
     @tags = @person.get_unique_tags(@person)
     @work = Work.new
     @title = "#{@person.first} #{@person.last}" 
+    #@contributor_group =  
   end
   
   def create
     parsed = params[:person][:name].split(" ")
     first = parsed[0]
     last = parsed[parsed.length - 1]
-	@person = Person.new(:title => params[:person][:title], :description => params[:person][:description],
+	  @person = Person.new(:title => params[:person][:title], :description => params[:person][:description],
 						 :first => first, :last => last)
-	if @person.save
-	  if !current_user.mentoree?(@person)
-		current_user.mentee!(@person)
-	  end
-	  redirect_to @person
-	else
-	  redirect_to suggest_path
+	  if @person.save
+	    if !current_user.mentoree?(@person)
+		  current_user.mentee!(@person)
+	    end
+	    redirect_to @person
+	  else
+	    redirect_to suggest_path
     end
   end
   
--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -3,8 +3,10 @@ class Contribution < ActiveRecord::Base
   
   belongs_to :contributed, :class_name => "Person"
   belongs_to :contributor, :class_name => "User"
+  belongs_to :work,        :class_name => "Work"
   
   validates :contributed_id, :presence => true
   validates :contributor_id, :presence => true
+  validates :work_id,        :presence => true
     
 end
